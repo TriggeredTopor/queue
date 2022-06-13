3eb18170a7d9f6d3f5b2fd055e20234209129357
TriggeredTopor <92325709+TriggeredTopor@users.noreply.github.com>
2022-06-13 20:44:15 +0300

    Update QueueImplementation.h

--- QueueImplementation.h
+++ QueueImplementation.h
@@ -5,16 +5,16 @@ using ValueType = double;
 
 class IQueueImplementation {
 public:
-
+    //Добавление в конец очереди
     virtual void push(const ValueType& value) = 0;
-
+    //Выкидываение первого 
     virtual void pop() = 0;
-
+    // просмотр первого в очереди
     virtual const ValueType& top() const = 0;
-
+    //проверка на пустоту
     virtual bool isEmpty() const = 0;
-
+    //Размер очереди
     virtual size_t size() const = 0;
-
+    //Деструктор
     virtual ~IQueueImplementation() = default;
 };
