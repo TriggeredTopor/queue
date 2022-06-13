54d808ae514a1e93b10496dea7221a6687b71a6b
TriggeredTopor <92325709+TriggeredTopor@users.noreply.github.com>
2022-06-13 20:01:13 +0300

    Create VectorQueue.cpp

---
+++ VectorQueue.cpp
@@ -0,0 +1,44 @@
+#include "Vector.h"
+
+VectorQueue::VectorQueue()
+{
+ 
+}
+
+void VectorQueue::push(const ValueType& value)
+{
+	Vector.push_back(value);
+}
+
+void VectorQueue::pop()
+{
+	for (size_t i = 0; i < Vector.size()-1; i++) {
+		Vector[i] = Vector[i + 1];
+	}
+	Vector.pop_back();
+}
+
+const ValueType& VectorQueue::top() const
+{
+	return (Vector.front());
+}
+
+bool VectorQueue::isEmpty() const
+{
+	if (Vector.size() != 0)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+size_t VectorQueue::size() const
+{
+	return (Vector.size());
+};
+
+VectorQueue::~VectorQueue()
+{
+	Vector.clear();
+}
