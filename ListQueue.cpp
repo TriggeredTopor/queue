34ff40ed3e0fb421e299d92101f67dfc916c28f8
TriggeredTopor <92325709+TriggeredTopor@users.noreply.github.com>
2022-06-13 20:01:37 +0300

    Create ListQueue.cpp

---
+++ ListQueue.cpp
@@ -0,0 +1,38 @@
+#include "ListQueue.h"
+
+ListQueue::ListQueue()
+{}
+
+void ListQueue::push(const ValueType& value)
+{
+	List.push_back(value);
+}
+
+void ListQueue::pop()
+{
+	List.pop_front();
+}
+
+const ValueType& ListQueue::top() const
+{
+	return(List.front());
+}
+
+bool ListQueue::isEmpty() const
+{
+	if (List.empty())
+	{
+		return true;
+	}
+	return false;
+}
+
+size_t ListQueue::size() const
+{
+	return (List.size());
+}
+
+ListQueue::~ListQueue()
+{
+	List.clear();
+}
