f53beefcfb56fb2af85ee0f63e764bc0d2132cf6
TriggeredTopor <92325709+TriggeredTopor@users.noreply.github.com>
2022-06-13 20:02:08 +0300

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,1 +1,25 @@
+#pragma once
+#include "QueueImplementation.h"
+#include <vector>
 
+using ValueType = double;
+using namespace std;
+
+class VectorQueue : public IQueueImplementation {
+private:
+    vector <ValueType> Vector;
+public:
+    VectorQueue();
+
+    void push(const ValueType& value)override;
+
+    void pop()override;
+
+    const ValueType& top() const override;
+
+    bool isEmpty() const override;
+
+    size_t size() const override;
+
+    virtual ~VectorQueue();
+};
